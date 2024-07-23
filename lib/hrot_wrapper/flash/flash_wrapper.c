4ef74ccca2f8bd7059dabe15bcc5f3256cb249a8
Steven Lee <steven_lee@aspeedtech.com>
2024-07-23 17:46:36 +0800

    coverity: CID 399795: Fix uninitialized pointer read
    Fixed uninitialized pointer read issue in
    Wrapper_spi_flash_get_block_size().
    
    Signed-off-by: Steven Lee <steven_lee@aspeedtech.com>
    Change-Id: I5083bfc9e2c40bf8aa82aeb71447554f2901feb9

--- lib/hrot_wrapper/flash/flash_wrapper.c
+++ lib/hrot_wrapper/flash/flash_wrapper.c
@@ -256,7 +256,7 @@ int Wrapper_spi_flash_sector_erase(struct spi_flash *flash, uint32_t sector_addr
  */
 int Wrapper_spi_flash_get_block_size(struct spi_flash *flash, uint32_t *bytes)
 {
-	struct flash_xfer xfer;
+	struct flash_xfer xfer = {0};
 
 	if ((flash == NULL) || (bytes == NULL)) {
 		return SPI_FLASH_INVALID_ARGUMENT;
