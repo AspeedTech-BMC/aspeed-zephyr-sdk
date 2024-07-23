11cfb5f0f9da5190519f71164eb360a5efffcf48
Steven Lee <steven_lee@aspeedtech.com>
2024-07-23 14:55:01 +0800

    board: Remove workaround for gpiot
    The default pinmux for GPIOTx group is set to GPIO in driver layer.
    
    Signed-off-by: Steven Lee <steven_lee@aspeedtech.com>
    Change-Id: I3f37c4994ed904eaca71655faaf24dc3091d721d

--- boards/arm/ast2700_dcscm/board.c
+++ boards/arm/ast2700_dcscm/board.c
@@ -31,19 +31,6 @@ static int ast2700_dcscm_init(void)
 	dev = device_get_binding("gpio0_e_h");
 	gpio_pin_configure(dev, 27, GPIO_OUTPUT_ACTIVE);
 #endif
-	// Workaround:
-	// Will be removed if zephyr sdk supports changing pin function to GPI Tx when ADC engine
-	// is disabled.
-#define ADC_ENGINE_CTRL    0x7e6e9000
-#define SCU_PIN_CTRL5      0x7e6e2430
-
-	uint32_t pinctrl_val = sys_read32(SCU_PIN_CTRL5);
-	uint32_t adc_engine_en = sys_read32(ADC_ENGINE_CTRL);
-	if (!(adc_engine_en & 1)) {
-		// Enable GPI T0 - T7
-		pinctrl_val |= 0xff000000;
-		sys_write32(pinctrl_val, SCU_PIN_CTRL5);
-	}
 
 	return 0;
 }
